70f08d504bb3acde39e3114dbe626bba3632a153
Mohamad <mohamad@myself>
2023-01-08 18:49:14 +0330

    feat: add excersie-2 compeletly.

--- mastering-distributed-tracing-by-yuri-shkuro/Chapter04/Exercise-2/Chapter04.Exercise_2/Program.cs
+++ mastering-distributed-tracing-by-yuri-shkuro/Chapter04/Exercise-2/Chapter04.Exercise_2/Program.cs
@@ -1,5 +1,10 @@
-using Chapter04.Exercise_1.Repositories;
+using Jaeger.Reporters;
+using Jaeger.Samplers;
+using Jaeger.Senders.Thrift;
+using Jaeger;
 using MySqlConnector;
+using OpenTracing;
+using Chapter04.Exercise_2.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +31,20 @@ builder.Services.AddSingleton(_ =>
 });
 
 builder.Services.AddScoped<MySqlRepository>();
+
+builder.Services.AddSingleton<ITracer>(sp =>
+{
+    var serviceName = "dotnet-2-hello";
+    var loggerFactory = sp.GetRequiredService<ILoggerFactory>();
+    var reporter = new RemoteReporter.Builder().WithLoggerFactory(loggerFactory)
+           .WithSender(new UdpSender()).Build();
+
+    var tracer = new Tracer.Builder(serviceName)
+           .WithSampler(new ConstSampler(true))
+           .WithReporter(reporter)
+           .Build();
+    return tracer;
+});
 
 
 var app = builder.Build();
---
+++ mastering-distributed-tracing-by-yuri-shkuro/Chapter04/Exercise-2/Chapter04.Exercise_2/Repositories/MySqlRepository.cs
@@ -0,0 +1,49 @@
+﻿using MySqlConnector;
+using OpenTracing;
+
+namespace Chapter04.Exercise_2.Repositories;
+
+public class MySqlRepository
+{
+    private readonly MySqlConnection _mySqlConnection;
+    private readonly ILogger<MySqlRepository> _logger;
+
+    public MySqlRepository(MySqlConnection mySqlConnection, ILogger<MySqlRepository> logger)
+    {
+        _mySqlConnection = mySqlConnection;
+        _logger = logger;
+        try
+        {
+            _mySqlConnection.Open();
+            _mySqlConnection.Ping();
+
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical("cannot connect to database", ex);
+        }
+    }
+
+
+    public async Task<string> GetHelloSentence(string name, ISpan span)
+    {
+        using var command = new MySqlCommand("select title, description from people where name =@name", _mySqlConnection);
+        command.Parameters.AddWithValue("@name", name);
+        using var reader = await command.ExecuteReaderAsync();
+        if (reader.Read())
+        {
+            span.Log(new List<KeyValuePair<string, object>>
+            {
+                new KeyValuePair<string, object>("name", name),
+                new KeyValuePair<string, object>("title", reader.GetValue(0)?.ToString()),
+                new KeyValuePair<string, object>("description", reader.GetValue(1)?.ToString()),
+            });
+            return $"Hello, {reader.GetValue(0)?.ToString()} {name}! {reader.GetValue(1)?.ToString()}";
+        }
+        else
+        {
+            return $"Hello, {name}!";
+        }
+    }
+
+}
