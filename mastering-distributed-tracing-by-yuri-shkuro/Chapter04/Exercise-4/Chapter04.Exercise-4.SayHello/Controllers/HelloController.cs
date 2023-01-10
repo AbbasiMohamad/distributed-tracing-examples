e5cc27660ac5abf8b963a75a54a2ad37c8b28efb
Mohamad <mohamad@myself>
2023-01-10 15:26:05 +0330

    refactor: remove some unnecessary files

--- mastering-distributed-tracing-by-yuri-shkuro/Chapter04/Exercise-4/Chapter04.Exercise-4.SayHello/Controllers/HelloController.cs
+++ mastering-distributed-tracing-by-yuri-shkuro/Chapter04/Exercise-4/Chapter04.Exercise-4.SayHello/Controllers/HelloController.cs
@@ -1,6 +1,6 @@
 using Chapter04.Exercise_4.SayHello.ExternalServices;
-using CommonLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
+using OpenTracing;
 
 namespace Chapter04.Exercise_4.SayHello.Controllers;
 [ApiController]
@@ -11,20 +11,25 @@ public class HelloController : ControllerBase
     private readonly ILogger<HelloController> _logger;
     private readonly BigBrotherService _bigBrotherService;
     private readonly FormatterService _formatterService;
+    private readonly ITracer _tracer;
 
     public HelloController(ILogger<HelloController> logger, BigBrotherService bigBrotherService,
-        FormatterService formatterService)
+        FormatterService formatterService, ITracer tracer)
     {
         _logger = logger;
         _bigBrotherService = bigBrotherService;
         _formatterService = formatterService;
+        _tracer = tracer;
     }
 
     [HttpGet("sayHello/{name}")]
     public async Task<string> SayHello([FromRoute] string name)
     {
+        var scope = _tracer.BuildSpan("say-hello").StartActive(true);
+        
         var person = await _bigBrotherService.GetPerson(name);
         var greetingString = await _formatterService.GetFormattedGretting(person);
+        scope.Dispose();
         return greetingString;
     }
 }
